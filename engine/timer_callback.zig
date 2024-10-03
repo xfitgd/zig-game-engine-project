94f995c5eb6d7b527d73c9f872b4a4e85385ba18
xfitgd <xfitgd@gmail.com>
2024-10-03 23:06:47 +0900

    timer_callback no spawn thread each callback function
    timer_callback no spawn thread each callback function

--- engine/timer_callback.zig
+++ engine/timer_callback.zig
@@ -1,68 +1,43 @@
 const std = @import("std");
 const system = @import("system.zig");
 
-const Self = @This();
-
-thread: std.Thread,
-
-inline fn loop(wait_nanosec: u64, comptime function: anytype, args: anytype) void {
+inline fn loop(wait_nanosec: u64, comptime function: anytype, args: anytype) bool {
     system.sleep(wait_nanosec);
-    _ = std.Thread.spawn(.{}, callback_, .{ function, args }) catch |err|
-        system.print_error("timer_callback loop : {s}\n", .{@errorName(err)});
+    return callback_(function, args);
 }
 
-inline fn loop2(wait_nanosec: u64, comptime function: anytype, args: anytype) void {
-    system.sleep(wait_nanosec);
-    if (@typeInfo(@typeInfo(@TypeOf(function)).@"fn".return_type.?) == .error_union) { // ? 표준 라이브러리 Thread.zig에서 가져온 코드
-        _ = @call(.auto, function, args) catch |err| {
-            system.print_error("ERR : {s}\n", .{@errorName(err)});
-        };
+fn callback_(comptime function: anytype, args: anytype) bool {
+    const res = @typeInfo(@typeInfo(@TypeOf(function)).@"fn".return_type.?);
+    if (res == .error_union) { // ? 표준 라이브러리 Thread.zig에서 가져온 코드
+        if (res.error_union.payload == bool) {
+            return @call(.auto, function, args) catch |err| {
+                system.print_error("ERR : {s}\n", .{@errorName(err)});
+                return false;
+            };
+        } else {
+            _ = @call(.auto, function, args) catch |err| {
+                system.print_error("ERR : {s}\n", .{@errorName(err)});
+                return false;
+            };
+        }
+    } else if (res == .bool) {
+        return @call(.auto, function, args);
     } else {
         _ = @call(.auto, function, args);
     }
+    return true;
 }
 
-fn callback_(comptime function: anytype, args: anytype) void {
-    if (@typeInfo(@typeInfo(@TypeOf(function)).@"fn".return_type.?) == .error_union) { // ? 표준 라이브러리 Thread.zig에서 가져온 코드
-        _ = @call(.auto, function, args) catch |err| {
-            system.print_error("ERR : {s}\n", .{@errorName(err)});
-        };
-    } else {
-        _ = @call(.auto, function, args);
-    }
-}
-
-fn callback(wait_nanosec: u64, repeat: u32, comptime function: anytype, args: anytype) void {
+fn callback(wait_nanosec: u64, repeat: u64, comptime function: anytype, args: anytype) void {
     var re = repeat;
     if (re == 0) {
-        while (true) {
-            loop(wait_nanosec, function, args);
-        }
+        while (loop(wait_nanosec, function, args)) {}
     } else {
-        while (re > 0) : (re -= 1) {
-            loop(wait_nanosec, function, args);
-        }
+        while (re > 0 and loop(wait_nanosec, function, args)) : (re -= 1) {}
     }
 }
 
-fn callback2(wait_nanosec: u64, repeat: u32, comptime function: anytype, args: anytype) void {
-    var re = repeat;
-    if (re == 0) {
-        while (true) {
-            loop2(wait_nanosec, function, args);
-        }
-    } else {
-        while (re > 0) : (re -= 1) {
-            loop2(wait_nanosec, function, args);
-        }
-    }
-}
-
-pub fn start(wait_nanosec: u64, repeat: u32, comptime function: anytype, args: anytype) std.Thread.SpawnError!std.Thread {
+///no spawn thread each callback function bool callback function return false or cause error -> exit timer
+pub fn start(wait_nanosec: u64, repeat: u64, comptime function: anytype, args: anytype) std.Thread.SpawnError!std.Thread {
     return try std.Thread.spawn(.{}, callback, .{ wait_nanosec, repeat, function, args });
-}
-
-///no spawn thread each callback function
-pub fn start2(wait_nanosec: u64, repeat: u32, comptime function: anytype, args: anytype) std.Thread.SpawnError!std.Thread {
-    return try std.Thread.spawn(.{}, callback2, .{ wait_nanosec, repeat, function, args });
 }
